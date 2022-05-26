f1b9a93672174033db823d13eac6339fa1d41947
WearierPuma01 <mr.bochka003@gmail.com>
2022-05-27 01:53:52 +0300

    Delete unused import

--- src/main/java/com/server/jourina/service/UserService.java
+++ src/main/java/com/server/jourina/service/UserService.java
@@ -12,7 +12,6 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
-import java.util.List;
 import java.util.Objects;
 
 @Service
