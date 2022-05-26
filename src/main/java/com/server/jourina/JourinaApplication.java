518ec40de4da5bef3a9518e31ac821aab56eb250
geniuspopeye <sasha.batalin@gmail.com>
2022-05-26 23:43:25 +0300

    hot fix

--- src/main/java/com/server/jourina/JourinaApplication.java
+++ src/main/java/com/server/jourina/JourinaApplication.java
@@ -2,6 +2,9 @@ package com.server.jourina;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class JourinaApplication {
@@ -9,5 +12,13 @@ public class JourinaApplication {
     public static void main(String[] args) {
         SpringApplication.run(JourinaApplication.class, args);
     }
-
+    @Bean
+    public WebMvcConfigurer corsConfig(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry){
+                registry.addMapping("/**");
+            }
+        };
+    }
 }
